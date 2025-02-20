2c8f6e3b8e0fbda657a92f9eb32cdee5ce035b6b
jige1024 <2576366072@qq.com>
2025-02-20 15:51:03 +0800

    鸡哥智能协同云图库项目 ---用户接口开发 --修复了分页查询时的bug --修复了预览时的数据精度丢失

--- src/main/java/com/jige/jigepicturebackend/common/DeleteRequest.java
+++ src/main/java/com/jige/jigepicturebackend/common/DeleteRequest.java
@@ -2,16 +2,18 @@ package com.jige.jigepicturebackend.common;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 通用的删除请求类
  */
 @Data
-public class DeleteRequest {
+public class DeleteRequest implements Serializable {
 
     /**
      * id
      */
     private Long id;
 
-    private static final Long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1837772666411107720L;
 }
