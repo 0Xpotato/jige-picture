b0297a0f14178f358bede8e69190849abcaa2e0f
jige1024 <2576366072@qq.com>
2025-03-25 14:54:02 +0800

    鸡哥智能协同云图库项目 ---团队空间 --创建团队共享空间接口和服务 --空间成员管理接口和服务

--- src/main/java/com/jige/jigepicturebackend/service/impl/SpaceUserServiceImpl.java
+++ src/main/java/com/jige/jigepicturebackend/service/impl/SpaceUserServiceImpl.java
@@ -21,6 +21,7 @@ import com.jige.jigepicturebackend.service.SpaceService;
 import com.jige.jigepicturebackend.service.SpaceUserService;
 import com.jige.jigepicturebackend.service.UserService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -44,6 +45,7 @@ public class SpaceUserServiceImpl extends ServiceImpl<SpaceUserMapper, SpaceUser
     private UserService userService;
 
     @Resource
+    @Lazy
     private SpaceService spaceService;
 
     /**
